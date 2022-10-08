d49ea37001d0943c3e627af187922aee76e38fda
ahmetzumber <ahmetzumber5@gmail.com>
2022-10-08 18:43:49 +0300

    [ Ahmet / Enes ] seed queries fixed.

--- seed/create_seed.sql
+++ seed/create_seed.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS users(
    created_at TIMESTAMP DEFAULT now(),
    updated_at TIMESTAMP DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS vocabulary(
+    id                      VARCHAR NOT NULL PRIMARY KEY,
+    word                    VARCHAR NOT NULL,
+    user_id                 VARCHAR NOT NULL references users ON DELETE CASCADE ON UPDATE CASCADE,
+    from_language           VARCHAR NOT NULL,
+    to_language             VARCHAR NOT NULL,
+    word_translation        VARCHAR,
+    created_at              TIMESTAMP DEFAULT now(),
+    updated_at              TIMESTAMP DEFAULT now()
+);
